c131b3cbd508be0f7a89a0fa1092c7640ef259cd
Sir_Hakim <52612878+sirHakim@users.noreply.github.com>
2022-06-09 23:50:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/spp/login-user.php');
+	header('Location: '.$uri.'/login-user.php');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
