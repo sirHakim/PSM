3dcff19845bcc8e8cb2b2ef831298dbcd94a6e9e
Sir_Hakim <52612878+sirHakim@users.noreply.github.com>
2022-06-10 01:14:08 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'spp_fsktm');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'EFH8GAqiK5');
+define('DB_PASSWORD', 'qHdrw4PFZD');
+define('DB_NAME', 'EFH8GAqiK5');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
